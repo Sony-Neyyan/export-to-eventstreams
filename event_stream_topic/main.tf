b6aa7accfdf7ee49fe7551bf2bbe19d5ab26f68e
Krishan Kumar <krisha19@gmail.com>
2021-05-06 12:30:22 +0530

    Added the event_stream_retention config to support the correct retention period in days

--- event_stream_topic/main.tf
+++ event_stream_topic/main.tf
@@ -1,7 +1,3 @@
-locals {
-  retension_period_in_ms = format("%d", (var.event_stream_retention * 86400000))
-}
-
 resource "ibm_resource_key" "resource_key" {
   name                 = "myresourcekey"
   role                 = "Writer"
@@ -18,4 +14,12 @@ resource "ibm_event_streams_topic" "es_topic" {
   resource_instance_id = var.streaming_lite_id
   name                 = var.event_stream_topic
   partitions           = 1
+  config = {
+     "retention.ms"    = format("%d", (var.event_stream_retention * 86400000))
+     "cleanup.policy"  = "delete"
+     "retention.bytes" = "1073741824"
+     "segment.bytes"   = "536870912"
+     "segment.ms" = "604800000"
+     "segment.index.bytes" = "10485760"
+  }
 }
